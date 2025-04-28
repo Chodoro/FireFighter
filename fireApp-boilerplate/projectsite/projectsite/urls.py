f46d4746af73d6ea5a0962f10bafb5c862d4c612
Eula14Euthanizer <139739789+Rosetta-Rigor@users.noreply.github.com>
2025-04-28 23:12:00 +0800

    iTS OvEr

--- fireApp-boilerplate/projectsite/projectsite/urls.py
+++ fireApp-boilerplate/projectsite/projectsite/urls.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 from fire import views
-from fire.models import Locations, Incident, FireStation
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-charts'),
+    path('chart/', PieCountbySeverity, name='charts'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
+
     path('stations', views.map_station, name='map-station'),
-    path('incidents', views.map_incident, name='map-incident'),
+    path('incidents', views.map_incidents, name='map-incidents'),
+
 
 ]
